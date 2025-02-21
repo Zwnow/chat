5ea90974e62efed7e88c6fad6415f00b9c874add
Zwnow <svenotimm@gmail.com>
2025-02-21 09:53:47 +0100

    docker-compose.yml update

--- user-service/config/config.go
+++ user-service/config/config.go
@@ -7,10 +7,5 @@ import (
 var PostgresURL string
 
 func LoadConfig() {
-	//err := godotenv.Load("/app/.env")
-	//if err != nil {
-	//	log.Fatalf("Error loading .env file: %v", err)
-	//}
-
 	PostgresURL = os.Getenv("POSTGRES_URL")
 }
